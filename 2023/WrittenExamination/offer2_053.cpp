ce2a359e9e8dfaae9f7c6fd60afe2afdad3f0ccb
tianenGit <415258758@qq.com>
2023-08-21 21:14:34 +0800

    8-21

---
+++ 2023/WrittenExamination/offer2_053.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    TreeNode* inorderSuccessor(TreeNode* root, TreeNode* p) {
+        stack<TreeNode*> st;
+        TreeNode *prev = nullptr, *curr = root;
+        while (!st.empty() || curr != nullptr) {
+            while (curr != nullptr) {
+                st.emplace(curr);
+                curr = curr->left;
+            }
+            curr = st.top();
+            st.pop();
+            if (prev == p) {
+                return curr;
+            }
+            prev = curr;
+            curr = curr->right;
+        }
+        return nullptr;
+    }
+};
