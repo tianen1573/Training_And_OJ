0b253d4af36ecfd241f1c2cd38c0181d98d61fc8
tianenGit <415258758@qq.com>
2023-09-11 22:06:54 +0800

    亿联-9.11

---
+++ 2023/PastPapers/亿联/1.cpp
@@ -0,0 +1,5 @@
+/*
+* 将一个字符串左移k位
+*/
+
+// https://leetcode.cn/problems/zuo-xuan-zhuan-zi-fu-chuan-lcof/?envType=study-plan-v2&envId=coding-interviews
---
+++ 2023/PastPapers/亿联/2.cpp
@@ -0,0 +1,8 @@
+/*
+* 约瑟夫环
+*/
+
+// https://blog.csdn.net/wu_tongtong/article/details/78323654
+
+// 循环链表
+// 数学公式：dp
